ee1733493d107683055a60eda7de3c4d53f6804a
Max <Mxoonka@gmail.com>
2026-08-07 20:45:10 +0300

    Fix missing FSMContext import in subscription handler.

--- bot/handlers/subscription.py
+++ bot/handlers/subscription.py
@@ -1,5 +1,6 @@
 from aiogram import F, Router
 from aiogram.filters import StateFilter
+from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, Message
 
 from bot.config import CURATOR_USERNAME, DB_PATH
